f76715c6ece45d7189f955d00f24b24f6fbfc9d4
USBrido <obrido@gmail.com>
2019-10-31 04:09:09 +0000

    First commit.

---
+++ which-school.js
@@ -0,0 +1,11 @@
+function whichSchool(num) {
+  if (num >= 13 && num <= 18) {
+    
+    return "Secondary School" ;
+  } else if (num < 13) {
+    return "Elementary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
+
